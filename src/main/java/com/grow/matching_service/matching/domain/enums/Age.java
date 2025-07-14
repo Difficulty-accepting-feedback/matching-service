14fb0ccbc815415661c644713258e68a1d5ef142
sun <suunn001@gmail.com>
2025-07-14 21:48:04 +0900

    :sparkles: feat: 연령대 enum 필드 추가
    erd 반영 완료

---
+++ src/main/java/com/grow/matching_service/matching/domain/enums/Age.java
@@ -0,0 +1,20 @@
+package com.grow.matching_service.matching.domain.enums;
+
+public enum Age {
+    TEENS("10대"),
+    TWENTIES("20대"),
+    THIRTIES("30대"),
+    FORTIES("40대"),
+    FIFTIES("50대"),
+    SIXTIES("60대 이상");
+
+    private final String description;
+
+    Age(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
