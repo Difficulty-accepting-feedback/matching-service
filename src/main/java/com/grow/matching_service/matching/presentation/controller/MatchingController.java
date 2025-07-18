67e75728e8062e412a14e39d2fbb115fcd9d4077
sun <suunn001@gmail.com>
2025-07-18 20:15:03 +0900

    :sparkles: feat: 매칭 목록 조회 컨트롤러 생성

--- src/main/java/com/grow/matching_service/matching/presentation/controller/MatchingController.java
+++ src/main/java/com/grow/matching_service/matching/presentation/controller/MatchingController.java
@@ -1,15 +1,16 @@
 package com.grow.matching_service.matching.presentation.controller;
 
+import com.grow.matching_service.matching.application.dto.MatchingResponse;
 import com.grow.matching_service.matching.application.service.MatchingService;
+import com.grow.matching_service.matching.domain.enums.Category;
 import com.grow.matching_service.matching.presentation.dto.MatchingRequest;
-import com.grow.matching_service.common.rsdata.RsData;
+import com.grow.matching_service.matching.presentation.dto.rsdata.RsData;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -23,7 +24,6 @@ public class MatchingController {
      * 매칭 정보를 저장합니다.
      *
      * @param request 매칭 요청 DTO
-     * @return 저장된 매칭 정보 DTO
      */
     @PostMapping("/save")
     public RsData<String> createMatching(@Valid @RequestBody MatchingRequest request) {
@@ -32,6 +32,28 @@ public class MatchingController {
         return new RsData<>(
                 "201",
                 "매칭 정보 생성 완료"
+        );
+    }
+
+    /**
+     * 카테고리를 기준으로 특정 회원의 매칭 정보를 전체 조회합니다.
+     *
+     * @param category 조회할 카테고리
+     * @param memberId 조회할 회원 ID
+     * @return 매칭 정보 DTO 리스트
+     */
+    @GetMapping("/check")
+    public RsData<List<MatchingResponse>> getAllMatching(@RequestParam("category") Category category,
+                                                         @RequestParam("memberId") Long memberId) {
+        log.info("[MATCH] 카테고리별 회원 매칭 목록 조회 요청 - category: {}, memberId: {}",
+                category, memberId);
+
+        List<MatchingResponse> responses = matchingService.getMatchingsByCategory(category, memberId);
+
+        return new RsData<>(
+                "200",
+                "회원별 매칭 정보 조회 완료",
+                responses
         );
     }
 }
