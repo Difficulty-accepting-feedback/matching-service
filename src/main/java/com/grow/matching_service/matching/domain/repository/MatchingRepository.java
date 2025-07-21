58a2d997488856df6e63fd66aabe47d509212235
sun <suunn001@gmail.com>
2025-07-21 16:34:21 +0900

    :sparkles: feat: 매칭 수정 레포지토리 로직 추가

--- src/main/java/com/grow/matching_service/matching/domain/repository/MatchingRepository.java
+++ src/main/java/com/grow/matching_service/matching/domain/repository/MatchingRepository.java
@@ -1,12 +1,14 @@
 package com.grow.matching_service.matching.domain.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.grow.matching_service.matching.domain.enums.Category;
 import com.grow.matching_service.matching.domain.model.Matching;
 
 public interface MatchingRepository {
-	void save(Matching matching);
+	Matching save(Matching matching);
 	List<Matching> findByMemberId(Long memberId);
 	List<Matching> findByCategoryAndMemberId(Category category, Long memberId);
+	Optional<Matching> findByMatchingId(Long matchingId);
 }
