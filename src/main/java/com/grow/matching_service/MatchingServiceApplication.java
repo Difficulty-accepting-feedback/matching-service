a2a589cc954f1d23871df5fcf76c043321201a09
sun <suunn001@gmail.com>
2025-07-23 11:33:34 +0900

    ✨ feat: FeignClient 를 이용하기 위해 설정 추가

--- src/main/java/com/grow/matching_service/MatchingServiceApplication.java
+++ src/main/java/com/grow/matching_service/MatchingServiceApplication.java
@@ -2,14 +2,15 @@ package com.grow.matching_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
+@EnableFeignClients
 @SpringBootApplication
 public class MatchingServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MatchingServiceApplication.class, args);
 	}
-
 }
