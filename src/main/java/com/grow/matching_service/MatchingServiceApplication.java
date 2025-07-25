be8a5daf250844d8bd854411ba3192b71d9a68e4
sun <suunn001@gmail.com>
2025-07-25 16:42:37 +0900

    ✨ feat: aop, 스케줄링 설정 추가

--- src/main/java/com/grow/matching_service/MatchingServiceApplication.java
+++ src/main/java/com/grow/matching_service/MatchingServiceApplication.java
@@ -3,10 +3,14 @@ package com.grow.matching_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAsync
+@EnableScheduling
 @EnableFeignClients
+@EnableAspectJAutoProxy
 @SpringBootApplication
 public class MatchingServiceApplication {
 
