00a7c2b6d7b185855d4b25c1e6c057b4bc6ac12b
sun <suunn001@gmail.com>
2025-08-04 23:45:25 +0900

    :sparkles: feat: 커스텀 오류 코드 및 글로벌 핸들러에 오류 처리 추가

---
+++ src/main/java/com/grow/matching_service/matching/application/exception/AlreadyDeletedException.java
@@ -0,0 +1,18 @@
+package com.grow.matching_service.matching.application.exception;
+
+import com.grow.matching_service.matching.presentation.exception.ErrorCode;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class AlreadyDeletedException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public AlreadyDeletedException(Throwable cause, ErrorCode errorCode) {
+        super(cause);
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+}
