ac40afcabb25c9b6ee8f262062468f1c4931274a
sun <suunn001@gmail.com>
2025-10-03 17:59:38 +0900

    ✅ test: 테스트 코드 수정

--- src/test/java/com/grow/matching_service/matching/infra/persistence/repository/MatchingTestV2.java
+++ src/test/java/com/grow/matching_service/matching/infra/persistence/repository/MatchingTestV2.java
@@ -9,12 +9,14 @@ import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ActiveProfiles("test")
 @SpringBootTest
 @Transactional
 public class MatchingTestV2 {
